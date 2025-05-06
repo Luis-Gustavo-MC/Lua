4950450d87b5c4b410b07b1636ec8053c410ce64
Luis Gustavo <lgmc120@gmail.com>
2025-05-05 23:35:42 -0300

    1

--- ex01/ex.lua
+++ ex01/ex.lua
@@ -7,9 +7,9 @@ repeat
     anoNascimento = tonumber(io.read())
     idade = 2025 - anoNascimento
     if anoNascimento == 2025 then
-        print("Ano não pode ser igual ao atual")
+        print("Ano nao pode ser igual ao atual")
         elseif idade>100 then
-            print("Idade não pode ser maior que 100")
+            print("Idade nao pode ser maior que 100")
     end
 until idade<=100 and anoNascimento ~= 2025
 --[[Saída de Dados--]]
